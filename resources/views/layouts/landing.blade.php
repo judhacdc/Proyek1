0dbb3a21231167b3c49b590da3aa7627af1d3c66
setyawan1234 <twentyzee1@gmail.com>
2022-04-11 12:31:38 +0700

    update copyright

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -82,7 +82,7 @@
         @yield('content')
         <!-- Footer-->
         <footer class="py-5 bg-dark ">
-            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Supraun Product 2021</p></div>
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Supraun Product 2022</p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
